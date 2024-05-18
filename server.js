f076a47a740072e9057f3f5798ffbe032aaf79c3
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-18 14:35:15 +0530

    changed the order of routes in server.js

--- server.js
+++ server.js
@@ -36,21 +36,9 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client", "./dist", "index.html"));
-});
-
-app.use("*", (req, res) => {
-  res.status(404).json({ msg: "not found" });
-});
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.get("/api/v1/test", (req, res) => {
   res.json({ msg: "test route" });
@@ -59,6 +47,14 @@ app.get("/api/v1/test", (req, res) => {
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client", "./dist", "index.html"));
+});
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
 
 app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5100;
